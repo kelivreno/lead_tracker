167e570c7512d943662ee92b7abcbb6acbca1d61
Kelangit Hakim <90366335+kelivreno@users.noreply.github.com>
2026-05-02 09:45:40 -0700

    Refactor variable naming for clarity in lead addition logic

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 
 inputBtn.addEventListener("click", function() {
-    new_input = inputEl.value
-    myLeads.push(new_input)
-    console.log(new_input)
+    newInput = inputEl.value
+    myLeads.push(newInput)
+    console.log(myLeads)
 })
