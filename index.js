5e6ccf8804d1ecd5fc76d5ac609754d66afc1f4e
Kelangit Hakim <90366335+kelivreno@users.noreply.github.com>
2026-05-02 09:51:19 -0700

    Add unordered list element for displaying leads

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     newInput = inputEl.value
     myLeads.push(newInput)
     console.log(myLeads)
 })
+
+for (let i = 0; i < myLeads.length; i++) {
+    console.log(myLeads[i])
+}
+
