90c6c21d76587c5467fb71b5b89f82d10840c288
Kelangit Hakim <90366335+kelivreno@users.noreply.github.com>
2026-05-04 09:44:37 -0700

    Refactor lead rendering to update the list with the latest input value directly

--- index.js
+++ index.js
@@ -6,23 +6,24 @@ const ulEl = document.getElementById("ul-el")
 inputBtn.addEventListener("click", function() {
     // newInput = inputEl.value
     myLeads.push(inputEl.value)
+    ulEl.innerHTML += "<li>" + inputEl.value + "</li>"
     inputEl.value = "" //clear the input field
     // console.log(myLeads)
-    renderLeads()
+    // renderLeads()
 })
 
-function renderLeads() {
-    let listItems = ""
-    // we prepare the listItems and afterwards, we render it so that the page doesn't 
-    // update every time there is a new link
-    // Alternative solution:
-    // let listItem = "<li>" + inputEl.value + "</li>"
-    // ulEl.innerHTML += listItem
+// function renderLeads() {
+//     let listItems = ""
+//     // we prepare the listItems and afterwards, we render it so that the page doesn't 
+//     // update every time there is a new link
+//     // Alternative solution:
+//     // let listItem = "<li>" + inputEl.value + "</li>"
+//     // ulEl.innerHTML += listItem
 
-    // for (let i = 0; i < myLeads.length; i++) {
-    //     listItems += "<li>" + myLeads[i]  + "</li>"
-    //     // console.log(listItems)
-    // }
-    let listItem = "<li>" + inputEl.value + "</li>"
-    ulEl.innerHTML = listItem
-}
+//     // for (let i = 0; i < myLeads.length; i++) {
+//     //     listItems += "<li>" + myLeads[i]  + "</li>"
+//     //     // console.log(listItems)
+//     // }
+//     let listItem = "<li>" + inputEl.value + "</li>"
+//     ulEl.innerHTML = listItem
+// }
