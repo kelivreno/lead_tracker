b663abc11e285784197a3b473420f8c735592954
Kelangit Hakim <90366335+kelivreno@users.noreply.github.com>
2026-05-04 09:38:57 -0700

    Add comments for clarity in input handling and lead rendering functions

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const ulEl = document.getElementById("ul-el")
 inputBtn.addEventListener("click", function() {
     // newInput = inputEl.value
     myLeads.push(inputEl.value)
-    inputEl.value = ""
+    inputEl.value = "" //clear the input field
     // console.log(myLeads)
     renderLeads()
 })
@@ -15,11 +15,14 @@ function renderLeads() {
     let listItems = ""
     // we prepare the listItems and afterwards, we render it so that the page doesn't 
     // update every time there is a new link
+    // Alternative solution:
+    // let listItem = "<li>" + inputEl.value + "</li>"
+    // ulEl.innerHTML += listItem
 
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i]  + "</li>"
-        // console.log(listItems)
-    }
-
-    ulEl.innerHTML = listItems
+    // for (let i = 0; i < myLeads.length; i++) {
+    //     listItems += "<li>" + myLeads[i]  + "</li>"
+    //     // console.log(listItems)
+    // }
+    let listItem = "<li>" + inputEl.value + "</li>"
+    ulEl.innerHTML = listItem
 }
