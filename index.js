0190626cd75ef560a7f21d1b8f470bac9fa62202
Kelangit Hakim <90366335+kelivreno@users.noreply.github.com>
2026-05-09 10:03:15 -0700

    Refactor leadsFromLocalStorage declaration to use const and improve delete button functionality

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsFromLocalStorage !== null) {
     myLeads = leadsFromLocalStorage
     renderLeads()
 }
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
 
 inputBtn.addEventListener("click", function() {
     // newInput = inputEl.value
