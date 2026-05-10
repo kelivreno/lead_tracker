1286c40c71067a8e71d29cb5d7070cde08c6dc6f
Kelangit Hakim <90366335+kelivreno@users.noreply.github.com>
2026-05-10 16:05:34 -0700

    Add retrieval of active tab details in tab button event listener

--- index.js
+++ index.js
@@ -19,6 +19,13 @@ const tabs = [
 ]
 
 tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        // since only one tab should be active and in the current window at once
+        // the return variable should only have one entry
+        let activeTab = tabs[0]
+        let activeTabId = activeTab.id // or do whatever you need
+    })
+
     // Save the url instead of logging it out
     myLeads.push(tabs[0].url)
     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
